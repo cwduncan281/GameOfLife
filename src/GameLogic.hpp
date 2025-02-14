1281317cf48d962efa177d3496d12dba1190eec9
cwduncan281 <cwduncan281@gmail.com>
2025-02-13 23:58:31 -0800

    initial refactoring

---
+++ src/GameLogic.hpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+namespace Game {
+
+class GameLogic {
+public:
+    void gameOfLife(std::vector<std::vector<int>>& board);
+};
+
+};
---
+++ src/Main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "GameLogic.hpp"
+
+int main(int argc, char** argv) {
+    auto thegame = new Game::GameLogic();
+
+    auto temp = new std::vector<std::vector<int>>();
+    // empty vector should cause no-op and clean return;
+    thegame->gameOfLife(*temp);
+
+    for (int i = 0; i < argc; i++) {
+        std::cout << argv[i] << std::endl;
+    }
+}
